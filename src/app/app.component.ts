a10b3ec92d8d349eac99be873a1cfac8460eb835
unknown <a_bavly@yahoo.com>
2022-10-05 13:34:54 +0200

    Fixing default local storage mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'online-store';
   mode: any;
+
+  ngOnInit(): void {
+  if (localStorage.getItem('mode')) {
+    let currentMode: any = localStorage.getItem('mode');
+    this.mode = JSON.parse(currentMode);
+  } else this.mode = true;
+}
   changeMode() {
     if (localStorage.getItem('mode')) {
       let currentMode: any = localStorage.getItem('mode');
